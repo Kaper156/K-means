0dbc7a496762f7cf37ca60f4711a8bf0be9f0ebb
kaper156 <156-oi@i.ua>
2017-03-04 23:02:15 +0100

    Worked version. Bugs: StackOverflow, not paint coordinate on <Form_load>

---
+++ K-means (WinForms)/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace K_means__WinForms_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(984, 706);
+            this.splitContainer1.SplitterDistance = 748;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(742, 700);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 184);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(226, 519);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "K-means";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+    }
+}
+
---
+++ K-means (WinForms)/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace K_means__WinForms_
+{
+    public partial class Form1 : Form
+    {
+        PictureBox canvas;
+
+
+        //Костыль СПРОСИТЬ
+        bool is_coordinated = false;
+
+        //Главный объект подсчетов
+        Misc.Work worker;
+
+        public Form1()
+        {
+            InitializeComponent();
+            worker = new Misc.Work();
+        }
+    
+
+        void init_coordinate_system()
+        {
+            canvas = pictureBox1;
+            
+            using (Graphics gdi = canvas.CreateGraphics())
+            {
+                gdi.Clear(Color.White);
+                int x = canvas.Width,
+                    y = canvas.Height;
+                gdi.DrawLine(Pens.Red, new Point(0, y / 2), new Point(x, y / 2));
+                gdi.DrawLine(Pens.Red, new Point(x / 2, 0), new Point(x / 2, y));
+            }
+            
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //КОСТЫЛЬ
+            if (!is_coordinated)
+            {
+                init_coordinate_system();
+                is_coordinated = true;
+            }
+
+            using (Graphics gdi = canvas.CreateGraphics())
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    gdi.FillEllipse(Brushes.Black, e.X, e.Y, 5, 5);
+                    this.worker.elements.Add(new Misc.Element(e.X, e.Y));
+                }
+                else
+                {
+                    gdi.FillEllipse(Brushes.Red, e.X, e.Y, 10, 10);
+                    this.worker.clusters.Add(new Misc.Cluster(e.X, e.Y));
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Костыльно
+            List<Brush> brushes = new List<Brush>();
+            brushes.Add(Brushes.Red);
+            brushes.Add(Brushes.Yellow);
+            brushes.Add(Brushes.Green);
+            brushes.Add(Brushes.Cyan);
+            brushes.Add(Brushes.Black);
+
+            init_coordinate_system();
+
+            foreach (Misc.Cluster cluster in worker.start())
+            {
+                //Костыльно
+                Brush brush = brushes[0];
+                brushes.RemoveAt(0);
+
+                using (Graphics gdi = canvas.CreateGraphics())
+                {
+                    gdi.FillEllipse(brush, (int)cluster.X, (int)cluster.Y, 10, 10); 
+                    foreach (Misc.Element element in cluster.elements)
+                    {
+                        gdi.FillEllipse(brush, (int)element.X, (int)element.Y, 5, 5); 
+                    }
+                }
+
+            }
+        }
+
+
+    }
+}
